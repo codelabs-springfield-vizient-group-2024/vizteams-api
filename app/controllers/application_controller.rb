eba1123a320b97f21bf0cef74fc7d1030424a901
jugglingdev <kaylamarie1785@gmail.com>
2024-04-28 22:39:35 -0500

    Add SessionsController and authentication method to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+    def authenticate_request
+        header = request.headers['Authorization']
+        header = header.split(' ').last if header
+        begin
+            decoded = JWT.decode(header, Rails.application.secret_key_base).first
+            @current_user = User.find(decoded['user_id'])
+        rescue JWT::ExpiredSignature
+            render json: { error: 'Token has expired' }, status: :unauthorized
+        rescue JWT::DecodeError
+            render json: { error: 'Unauthorized' }, status: :unauthorized
+        end
+    end
 end
