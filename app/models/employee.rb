0a5c84e0590c5f8712d7dfcfb977e5b1e14ddd65
Paul Theby <paultheby@gmail.com>
2024-04-29 20:31:31 -0500

    fixed seeding

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,7 +2,9 @@ class Employee < ApplicationRecord
     validates :first_name, presence: true
     validates :last_name, presence: true
   
-
+    has_many :employee_teams
     belongs_to :job_title
-    has_many :teams, through: :employee_teams
+   
+    has_many   :teams, through: :employee_teams
+   
 end
--- app/models/employee_team.rb
+++ app/models/employee_team.rb
@@ -1,4 +1,5 @@
 class EmployeeTeam < ApplicationRecord
-    belongs_to :employees
+    belongs_to :employee
+    
     belongs_to :team
 end
