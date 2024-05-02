918a9aa7ed6a46cdab9a9cc74db77d3eb2718475
Paul Theby <paultheby@gmail.com>
2024-05-02 18:22:19 -0500

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
+    has_secure_password
+    
     validates :first_name, presence: true
     validates :last_name, presence: true
     validates :email, presence: true, uniqueness: true
-    validates :password, presence: true
-    validates :password_confirmation, presence: true
+    validates :password_digest, presence: true
 end
