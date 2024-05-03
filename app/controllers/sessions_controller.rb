e80c4fb30a1fa3dfcf3aeb4d656f0197027c45ed
mcr45 <depr3604@protonmail.com>
2024-05-02 19:48:37 -0500

    signup work login not working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   # POST /login
   def create
     user = User.find_by(email: params[:email])
-    if user&.authenticate(params[:password])
+    if user&.authenticate()
       token = jwt_encode(user_id: user.id)
       render json: { token: token }, status: :ok
     else
