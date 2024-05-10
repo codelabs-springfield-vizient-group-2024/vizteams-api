86e0b74020a9e19f4fb5941273a91fb82c0bba2e
Paul Theby <paultheby@gmail.com>
2024-05-09 19:21:42 -0500

    Delete 20240509234646_remove_password_and_confirmation_from_users.rb

--- db/migrate/20240509234646_remove_password_and_confirmation_from_users.rb
+++
@@ -1,6 +0,0 @@
-class RemovePasswordAndConfirmationFromUsers < ActiveRecord::Migration[7.1]
-  def change
-    remove_column :users, :password
-    remove_column :users, :password_confirmation
-  end
-end
