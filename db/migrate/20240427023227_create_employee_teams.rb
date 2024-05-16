2bcd8616633119bd8a6e2212b59583820017bce4
Paul Theby <paultheby@gmail.com>
2024-05-16 17:54:33 -0500

    Update 20240427023227_create_employee_teams.rb
    remove foreign key on delete nulligy within the create employee teams migration file

--- db/migrate/20240427023227_create_employee_teams.rb
+++ db/migrate/20240427023227_create_employee_teams.rb
@@ -4,7 +4,7 @@ class CreateEmployeeTeams < ActiveRecord::Migration[7.1]
       t.date :start_date
       t.date :end_date
       t.references :employee, null: false
-      t.references :team, null: false, foreign_key: { on_delete: :nullify }
+      t.references :team, null: false
 
       t.timestamps
     end
