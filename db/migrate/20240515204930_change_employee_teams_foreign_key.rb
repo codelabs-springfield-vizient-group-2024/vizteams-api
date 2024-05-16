4164a471d76c240dfc34a05010b1a69e35cf64e6
Paul Theby <paultheby@gmail.com>
2024-05-16 17:50:17 -0500

    delete duplicate migrations
    deleting dup migrations that both add foreign key for employee_teams adding team FK

--- db/migrate/20240515204930_change_employee_teams_foreign_key.rb
+++
@@ -1,6 +0,0 @@
-class ChangeEmployeeTeamsForeignKey < ActiveRecord::Migration[7.1]
-  def change
-    remove_foreign_key :employee_teams, :teams
-    add_foreign_key :employee_teams, :teams, on_delete: :nullify
-  end
-end
--- db/migrate/20240515205526_add_foreign_key_constraint_to_employee_teams.rb
+++
@@ -1,5 +0,0 @@
-class AddForeignKeyConstraintToEmployeeTeams < ActiveRecord::Migration[7.1]
-  def change
-    add_foreign_key :employee_teams, :teams, on_delete: :cascade
-  end
-end
