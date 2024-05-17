897bc45f357206450844474674a1419409cd5adc
Paul Theby <paultheby@gmail.com>
2024-05-16 19:12:00 -0500

    Update 20240427023227_create_employee_teams.rb
    REMOVE employee and team foreign keys

--- db/migrate/20240427023227_create_employee_teams.rb
+++ db/migrate/20240427023227_create_employee_teams.rb
@@ -3,8 +3,7 @@ class CreateEmployeeTeams < ActiveRecord::Migration[7.1]
     create_table :employee_teams do |t|
       t.date :start_date
       t.date :end_date
-      t.references :employee, null: false
-      t.references :team, null: false
+    
 
       t.timestamps
     end
