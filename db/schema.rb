b5c91863242b33ba86aa4686cf94c33ea82ccf9e
jugglingdev <kaylamarie1785@gmail.com>
2024-05-20 20:31:25 -0500

    Create employees_teams record when creating employee

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_05_16_231136) do
-
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
