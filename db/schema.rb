b9b1c995c9b57c4daf125a8d26bbd148bc48c4cf
Grant Reese <119977613+GrantReese@users.noreply.github.com>
2024-05-21 17:06:07 -0500

    added profile method
    pictures still not showing

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_05_16_231136) do
-    create_table "active_storage_attachments", force: :cascade do |t|
+  create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
     t.bigint "record_id", null: false
