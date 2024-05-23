20b62ec47e758e0c9f15fae0a8685df05e896cce
Paul Theby <paultheby@gmail.com>
2024-05-23 16:17:20 -0500

    remove comma

--- db/schema.rb
+++ db/schema.rb
@@ -85,7 +85,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_16_231136) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "employee_teams", "employees"
-  add_foreign_key "employee_teams", "teams",
+  add_foreign_key "employee_teams", "teams"
 
   add_foreign_key "employees", "job_titles"
 end
