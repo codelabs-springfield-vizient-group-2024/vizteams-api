27c5c8e7c7ba6ee8e459d650fbb7e1aa1ad300f2
Paul Theby <paultheby@gmail.com>
2024-05-24 21:35:46 -0500

    Updated resorting persists
    removed duplicate
      add_foreign_key "employee_teams", "teams"

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
 ActiveRecord::Schema[7.1].define(version: 2024_05_23_234108) do
-
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -90,5 +87,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_23_234108) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "employee_teams", "employees"
   add_foreign_key "employee_teams", "teams"
+ 
   add_foreign_key "employees", "job_titles"
 end
