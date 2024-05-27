db29a9927dcec58838a44950bb6ee59ea9131c6c
Paul Theby <paultheby@gmail.com>
2024-05-27 13:25:20 -0500

    add job_title as association on blueprinter

--- app/blueprints/employee_blueprint.rb
+++ app/blueprints/employee_blueprint.rb
@@ -3,5 +3,8 @@
 class EmployeeBlueprint < Blueprinter::Base
   identifier :id
    
-  fields :first_name, :last_name, :job_title, :profile_image_url
+  fields :first_name, :last_name, :profile_image_url
+  view :normal do
+    association :job_title, blueprint: JobTitleBlueprint, view: :normal
+  end
 end
